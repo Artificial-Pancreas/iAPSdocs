d6e66b72e2ddda704c75379d281a8b767ace1da6
sethgagnon <sethgagnon@gmail.com>
2023-03-21 19:25:33 -0400

    editing themes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,8 +34,15 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
+    'myst_parser',
     # 'alabaster',
 ]
+
+myst_enable_extensions = [
+  "colon_fence",
+]
+
+myst_heading_anchors = 3
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -68,7 +75,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
